d4d012717d1f55009557eb018ae5f89771ae6fc2
Anjli <anjlipalbest@gmail.com>
2024-06-21 15:23:52 +0530

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './common/footer/footer.component';
+import { HeaderComponent } from './common/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { ResumeComponent } from './component/resume/resume.component';
+import { PricingComponent } from './component/pricing/pricing.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { FeaturesComponent } from './component/features/features.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ClientsComponent } from './component/clients/clients.component';
+import { BlogComponent } from './component/blog/blog.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,
+  HomeComponent,ResumeComponent,PricingComponent,PortfolioComponent,FeaturesComponent,ContactComponent,ClientsComponent,BlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
