719cd15ebe28b2f6628306c5c7be2c8887065a8e
vijaypalbhagel <vijaypalbhagel@gmail.com>
2024-06-26 09:57:56 +0530

    developer and coder

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy,PLATFORM_ID  } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+ 
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy  {
+  bannerTxt: any='Developer'
+  private intervalId: any;
+  className:any='typing-animation'
 
+  constructor(@Inject(PLATFORM_ID) private platformId: object) { }
+
+  ngOnDestroy(): void {
+    this.clearInterval();
+  }
+
+
+
+
+
+  
+
+  
+
+   startInterval(): void {
+    this.intervalId = setInterval(() => {
+     console.log('timer start')
+     this.bannerTxt= this.bannerTxt=='Developer'?'Professional Coder':'Developer'
+     if(this.bannerTxt =='Developer'){
+      this.className='typing-animation'
+     }else{
+      this.className=''
+       this.className='typing-animation2'
+     }
+    }, 3000); 
+  }
+
+   clearInterval(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ngOnInit(): void {
+ 
+    if (isPlatformBrowser(this.platformId)) {
+      document.getElementById('start')?.click()
+    }
+    console.log('ngOnInit called');
+   
+  }
+
+
+ 
+
+  // ngOnDestroy(): void {
+  //   console.log('ngOnDestroy called');
+  // }
+ 
 }
