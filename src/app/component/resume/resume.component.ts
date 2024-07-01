d05c1a3340027b0ec436fa07af46584ca5427522
Anjli <anjlipalbest@gmail.com>
2024-07-01 21:11:11 +0530

    add

--- src/app/component/resume/resume.component.ts
+++ src/app/component/resume/resume.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-
+title = 'Bytes';
+activeTab:string = 'Education'
+  onTabClick(tab:any){
+    this.activeTab = tab; 
+  }
+  
 }
