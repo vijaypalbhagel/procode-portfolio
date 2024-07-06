f9cd5acf04a9ae6c5b03059ca688c200941dc1fb
Anjli <anjlipalbest@gmail.com>
2024-07-06 21:57:20 +0530

    add

--- src/app/component/blog/blog.component.ts
+++ src/app/component/blog/blog.component.ts
@@ -8,7 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+  modelDate: any= ''
+  modalImage: any=''
   addProduct(){
     
+  }
+  openModal(num: any){
+    if(num ==1){
+      this.modelDate='06/07/2024'
+      this.modalImage = "../../../assets/blog-01.webp"
+    }else if(num ==2){
+      this.modelDate='08/08/2024'
+      this.modalImage = "../../../assets/blog-02.webp"
+    }else if(num ==3){
+      this.modelDate='20/05/2023'
+      this.modalImage = "../../../assets/blog-03.webp"
+    }
   }
 }
