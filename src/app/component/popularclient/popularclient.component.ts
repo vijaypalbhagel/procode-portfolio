d78eeb000b2f00322ef0fdc3700ccb6bd1e8e3cc
Anjli <anjlipalbest@gmail.com>
2024-07-08 23:25:46 +0530

    add

--- src/app/component/popularclient/popularclient.component.ts
+++ src/app/component/popularclient/popularclient.component.ts
@@ -10,26 +10,32 @@ import { CommonModule } from '@angular/common';
   styleUrl: './popularclient.component.css'
 })
 export class PopularclientComponent {
-  showall=false;
-  designsvalue=false;
-  lastvalue:any='false';
-  script(){
-    
-  }
+ popularimg:any=''
  
-  all(){
-this.showall=true;
-  }
-  Design(){
-this.designsvalue=true;
-  }
-  word(){
 
-  }
-  react(){
 
-  }
-  last(){
-this.lastvalue=true;
+  scriptall(event:any,num:any){
+    console.log(num)
+    event.preventDefault();
+ if(num==1){
+  this.popularimg = '../../../assets/client1.png'
+  this.popularimg = '../../../assets/client2.png'
+  this.popularimg ='../../../assets/client3 (1).png'
+ }
+ else if(num==2){
+  this.popularimg = '../../../assets/client3.png'
+ }
+ else if(num==3){
+  this.popularimg = '../../../assets/client1.png'
+  this.popularimg = '../../../assets/client2.png'
+ }
+ else if(num==4){
+  this.popularimg ='../../../assets/client3 (1).png'
+ }
+ else if(num==5){
+  this.popularimg = '../../../assets/client2.png'
+ }else if(num==6){
+  this.popularimg = '../../../assets/client1.png'
+ }
   }
 }
